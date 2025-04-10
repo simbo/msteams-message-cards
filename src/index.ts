6e8e03faf5f15c006d14232ad61e0471548baedd
Simon Lepel <simon.lepel@mercedes-benz.com>
2025-04-10 16:25:39 +0200

    update tsconfig and fix issues

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-export { sendMessageCard } from './send-message-card.js';
 export { createMessageCardPayload } from './create-message-card-payload.js';
+export { sendMessageCard } from './send-message-card.js';
 
-export { validateMessageCardPayload } from './validate-message-card-payload.js';
 export { validateMessageCardOptions } from './validate-message-card-options.js';
 export { validateMessageCardPayloadOptions } from './validate-message-card-payload-options.js';
+export { validateMessageCardPayload } from './validate-message-card-payload.js';
 
 export {
-  payloadSchema,
+  payloadFactSchema,
   payloadPotentialActionSchema,
-  payloadSectionSchema,
-  payloadFactSchema
+  payloadSchema,
+  payloadSectionSchema
 } from './payload.schema.js';
 
 export {
-  optionsSchema,
-  optionsButtonSchema,
-  optionsSectionSchema,
   optionsActivitySchema,
+  optionsButtonSchema,
   optionsFactSchema,
+  optionsSchema,
+  optionsSectionSchema,
   payloadOptionsSchema
 } from './options.schema.js';
 
-export { Payload, PayloadSection, PayloadFact, PayloadPotentialAction } from './payload.interface.js';
+export type { Payload, PayloadFact, PayloadPotentialAction, PayloadSection } from './payload.interface.js';
 
-export {
+export type {
   Options,
-  OptionsSection,
   OptionsActivity,
-  OptionsFact,
   OptionsButton,
+  OptionsFact,
+  OptionsSection,
   PayloadOptions
 } from './options.interface.js';
